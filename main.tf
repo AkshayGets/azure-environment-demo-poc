16375edb6b81b3a69273e3f36e9f1f6ba48c64d0
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2020-05-18 15:42:44 +0530

    added remote state content into main.tf file

--- main.tf
+++ main.tf
@@ -7,3 +7,11 @@ resource "azurerm_resource_group" "demo" {
   name     = "first-steps-demo"
   location = var.location
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "tfstoragetrainingjorn"
+    container_name       = "terraform-state-container"
+    key                  = "terraform.tfstate"
+  }
+}
